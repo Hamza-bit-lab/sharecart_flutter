0fdd50b5ba26cdc3910adc4c9ab593c3dc1e198e
Hamza-bit-lab <hamzarashid7574@gmail.com>
2026-04-03 16:36:12 +0300

    added production url

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,2 +1,3 @@
 // const String apiBaseUrl = 'http://192.168.100.180:8000';
-const String apiBaseUrl = 'http://127.0.0.1:8000';
+// const String apiBaseUrl = 'http://127.0.0.1:8000';
+const String apiBaseUrl = 'https://share-carts.com';
